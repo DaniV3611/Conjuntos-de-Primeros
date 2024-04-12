40fa2250195258aa5ab1cf42ae721418a147f1eb
daniv <danivego2015@gmail.com>
2024-04-12 16:20:09 -0500

    Conjunto de primeros corregido

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 import sys
 
 def leerArchivo():
-	# Lee un archivo de python
+	#Lee un archivo de python
 	if len(sys.argv) != 2:
-	    print("Uso: python3 Main.py <archivo.py>")
-	    sys.exit(1)
-
-	# Obtener el nombre del archivo del primer argumento
+		print('Uso: python3 Main.py <archivo.txt>')
+		sys.exit(0)
+	
+	#Obtener el nombre del archivo del primer argumento
 	python_file = sys.argv[1]
 
 	archivo = []
@@ -51,43 +51,89 @@ def leerGramatica (archivo):
 
 	return reglas
 
-def calcularPrimero(simbolo_no_terminal, reglas):
+# Calcula los primeros de una regla especifica
+def calcularPrimero(simbolo_no_terminal, regla, reglas):
+
 	primeros = []
-	# Se analiza el primer simbolo de cada una de las reglas
-	for  regla in reglas.get(simbolo_no_terminal):
 
-		# print(f"Regla: {regla}")
-		
-		# Si la regla es vacia
-		if len(regla) == 0:
-			if '/epsilon' not in primeros:
-				primeros.append('/epsilon')
-		# Si el simbolo es terminal
-		elif regla[0] not in reglas:
-			if regla[0] not in primeros:
-				primeros.append(regla[0])
+	# Si la regla esta vacia
+	if len(regla) == 0:
+		primeros.append('/epsilon')
+		return primeros
+
+	# Se analiza el primer simbolo de la regla
+	
+	# Si el primer simbolo es terminal
+	if regla[0] not in reglas:
+		primeros.append(regla[0])
+		return primeros
+	
+	# Si el primer simbolo es NO terminal
+ 
+	# Guardar los no_terminales encontrados
+	primeros_no_terminales = []
+	
+	# Se busca si este tiene mas simbolos NO terminales detras
+	for simbolo in regla:
+		if simbolo in regla:
+			# Si el no_terminal es el simbolo del que se esta calculando se ignora
+			if simbolo != simbolo_no_terminal:
+				temp = []
+				# Se recorre cada regla del no_terminal para buscar sus primeros
+				for regla_no_terminal in reglas[simbolo]:
+					temp.append(calcularPrimero(simbolo, regla_no_terminal, reglas))
+
+				# Se guardan los primeros encontrados
+				lista = []
+				for encontrado in temp:
+					for simb in encontrado:
+						# Se eliminan los repetidos
+						if simb not in lista:
+							lista.append(simb)
+
+				primeros_no_terminales.append(lista)
 		else:
-			if regla[0] != simbolo_no_terminal:
-				for primero in calcularPrimero(regla[0], reglas):
-					if primero == '/epsilon':
-						if len(regla) == 1:
-							if primero not in primeros:
-								primeros.append(primero)
-						else:
-							continue
-					if primero not in primeros:
-						primeros.append(primero)
+			break
 
+	# Se guardan los primeros encontrados en los primeros que se estan calculando
+	agregar_vacio = True
+
+	for primeros_encontrados in primeros_no_terminales:
+
+		# Si algun conjunto no contiene vacio, este no se agregara
+		if '/epsilon' not in primeros_encontrados:
+			agregar_vacio = False
+
+		for simbol in primeros_encontrados:
+			if simbol not in primeros and simbol != '/epsilon':
+				primeros.append(simbol)
+		
+	if agregar_vacio:
+		primeros.apend('/epsilon')
+	
 	return primeros
 
 def calcularPrimeros(reglas):
 
 	conjunto_primeros = {}
+	conjunto_primeros_reglas = []
 
 	for no_terminal in list(reglas.keys()):
-		conjunto_primeros[no_terminal] = calcularPrimero(no_terminal, reglas)
+		temp = []
+		for regla in reglas[no_terminal]:
+			primeros = calcularPrimero(no_terminal, regla, reglas)
+			conjunto_primeros_reglas.append(primeros)
+			temp.append(primeros)
 
-	return conjunto_primeros
+		lista = []
+		for elemento in temp:
+			for simbol in elemento:
+				if simbol not in lista:
+					lista.append(simbol)
+
+		conjunto_primeros[no_terminal] = lista
+
+	return conjunto_primeros, conjunto_primeros_reglas
 
 def imprimir(conjunto):
 	for no_terminal, primeros in conjunto.items():
@@ -98,5 +144,5 @@ archivo = leerArchivo()
 
 reglas = leerGramatica(archivo)
 
-primeros = calcularPrimeros(reglas)
+primeros, conjunto_primeros_reglas = calcularPrimeros(reglas)
 imprimir(primeros)
